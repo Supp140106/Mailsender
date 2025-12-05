6f59cc4150d70e32d488776726965019cc35f5d1
Supp140106 <suppritdas@gmail.com>
2025-12-05 10:22:34 +0530

    error fixing

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",        // allow all origins
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
+
 app.use(express.json());
 
 // Nodemailer Transporter
