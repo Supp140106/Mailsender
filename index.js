64aa3f52df4c1dba9000ffe64de129320851fc2d
Supp140106 <suppritdas@gmail.com>
2025-12-05 11:14:16 +0530

    smtp error fixed test

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
-    secure: false, // true for 465, false for other ports
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, // Use App Password, not regular password
